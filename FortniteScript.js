73a81f1bc3fa42d9fbec108e5daf6d8a5a0d376a
Tom Shahar <tom.e.shahar@gmail.com>
2018-03-16 10:10:17 -0700

    animations, organization

--- FortniteScript.js
+++
@@ -1,4 +0,0 @@
-var cascade = []
-function cascade() {
-  
-}
--- Parallax.js
+++ Parallax.js
@@ -1,18 +1,63 @@
-import * as basicScroll from 'basicscroll'
+/*document.getElementById('pre__loadin').onscroll = function() {
+  console.log("scrolling");
+};
+*/
 
-const instance = basicScroll.create({
-   elem: document.querySelector('.icon__anim'),
-   from: 'top-top',
-   to: 'bottom-bottom',
-   direct: true,
-   props: {
-      '--r': {
-         from: '0',
-         to: '1turn'
-      },
-      '--tx': {
-         from: '-100px',
-         to: '100px'
-      }
-   }
-})
+  /* from 0-100px arrow wrapper and title container are translated down 100px, with the scroll;
+  after 100px they are translated upvery quickly and pre__loadin is set to display none
+  while main__loadin is set to display block*/
+  /*
+  const instances = []
+  instances.push(basicScroll.create({
+  	elem: document.querySelector('.test'),
+  	from: '100vh',
+  	to: '150vh',
+  	props: {
+  		'--opacity': {
+  			from: .01,
+  			to: .99
+  		}
+  	}
+  }))
+  instance.start()
+  */
+function testScroll(ev){
+    if(window.pageYOffset>1)loadin__start();
+}
+window.onscroll=testScroll
+
+function loadin__start(){
+  console.log("working");
+  document.getElementById('pre__loadin').style.opacity = "0";
+
+  /*document.getElementById('title__wrap').style.opacity = "0";*/
+  document.getElementById('title__wrap').setAttribute('style', 'transform: scale(5,5); opacity: 0');
+  document.getElementById('arrow__wrapper').setAttribute('style', 'padding-top: 0vh');
+  document.getElementById('main__loadin').style.display = "block";
+
+
+  setTimeout(loadin__start__2, 1000);
+}
+
+function loadin__start__2(){
+  console.log("huh");
+  document.getElementById('pre__loadin').style.left = "100vh";
+  document.getElementById('arrow__wrapper').setAttribute('style', 'display: none');
+  document.getElementById('pre__loadin').setAttribute('style', 'opacity: 0.01');
+  /*document.getElementById('pre__loadin').setAttribute('style', 'background-attachment: scroll; transform: scale(0.1);');*/
+
+  setTimeout(loadin__start__3, 1000);
+}
+function loadin__start__3(){
+  document.getElementById('pre__loadin').style.display = "none";
+  document.getElementById('main__loadin').style.opacity = "1";
+}
+
+
+
+
+
+/*
+
+
+*/
